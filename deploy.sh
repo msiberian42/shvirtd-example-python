953e26096baebd53c34fd4bc6654c644ece67395
msiberian42 <120947225+msiberian42@users.noreply.github.com>
2026-08-24 21:11:47 +0300

    Change project directory to /opt/my_servicee

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 REPO_URL="https://github.com/msiberian42/shvirtd-example-python.git"
-PROJECT_DIR="$HOME/my_service"
+PROJECT_DIR="/opt/my_service"
 
 git clone "$REPO_URL" "$PROJECT_DIR"
 
