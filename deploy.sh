c3dcf9b20237df9f19d15f54bd9abf489ccf38db
Vboxuser <pablo@linux.com>
2026-08-24 18:11:00 +0000

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+REPO_URL="https://github.com/msiberian42/shvirtd-example-python.git"
+PROJECT_DIR="$HOME/my_service"
+
+git clone "$REPO_URL" "$PROJECT_DIR"
+
+cd "$PROJECT_DIR"
+
+docker compose up -d
